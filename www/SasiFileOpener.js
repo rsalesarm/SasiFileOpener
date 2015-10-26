7d646f070d81353d5a4946414d57ca53f2bea36b
Sasidharan <madhavanindira@users.noreply.github.com>
2015-10-26 14:33:03 +0530

    Update SasiFileOpener.js

--- www/SasiFileOpener.js
+++ www/SasiFileOpener.js
@@ -1,1 +1,12 @@
 
+module.exports = {
+    open: function (url) {
+        var success = function () {
+            console.log("success!");
+        }
+        var failure = function (error) {
+            console.log(error);
+        }
+        cordova.exec(success, failure, "SasiFileOpener", "openFile", [url]);
+    }
+}
