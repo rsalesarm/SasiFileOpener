3caaa19a2fad9e915a38eb06950cbfa2ee2c0aa9
Sasidharan <madhavanindira@users.noreply.github.com>
2015-10-26 14:36:56 +0530

    Update SasiFileOpener.h

--- src/ios/SasiFileOpener.h
+++ src/ios/SasiFileOpener.h
@@ -1,1 +1,12 @@
 
+#import <Cordova/CDV.h>
+
+@interface SasiFileOpener : CDVPlugin <UIDocumentInteractionControllerDelegate> {
+    NSString *localFile;
+}
+
+@property(nonatomic, strong) UIDocumentInteractionController *controller;
+
+- (void) openFile: (CDVInvokedUrlCommand*)command;
+
+@end
